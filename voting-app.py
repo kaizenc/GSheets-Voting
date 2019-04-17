e2bb98958e516d0ad276c46c40ef7d6d1acdf690
Kaizen Castanos <kaizen.castanos@narrativ.com>
2019-04-17 09:29:00 -0400

    Updated for Noemie dropping, using regex and added requirements

--- voting-app.py
+++ voting-app.py
@@ -4,6 +4,8 @@ import sys
 import voteFuncs
 from oauth2client.service_account import ServiceAccountCredentials
 
+import re
+
 # Use creds to create a client to interact with the Google Drive API
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
@@ -32,28 +34,25 @@ if __name__ == "__main__":
 	temp = input()
 	
 	# Temporary; if you have any names you don't want to accept, enter them here
-	blacklist = []
+	blacklist = ['']
 
 	# Final counted votes
 	final_votes = [i for i in list_1 if (i[1].title() not in blacklist and i[1].title() in names)] # remove
 
-	ec_candidates = ['Andrew Jreza', 'Justin Wu']
+	first_row = list_1[0]
+	regEx = "\[(.*?)\]"
+
 	ec_vote_cols = [2]
+	ec_candidates = ['Andrew Jreza', 'Justin Wu']
 	ec_result = voteFuncs.vote_2("Event Coordinator", ec_candidates, ec_vote_cols, final_votes)
-
-	ePR_candidates = ['Marie Guanzon', 'Andrew Jreza', 'JD Pacamarra']
-	ePR_vote_cols = [3, 4, 5]
-	ePR_result = voteFuncs.vote_3("External PR", ePR_candidates, ePR_vote_cols, final_votes)
-
-	# TODO: Implement vote_4
-	# iPR_candidates = ['Marie Guanzon', 'Mara Lopez', 'David Luu', 'Andy + Raymond']
-	# iPR_vote_cols = [6, 7, 8, 9]
-	# iPR_result = voteFuncs.vote_4("Internal PR", iPR_candidates, iPR_vote_cols, final_votes)
-
-	vp_candidates = ['Sam Hidalgo', 'Noemie Li']
-	vp_vote_cols = [10]
-	vp_result = voteFuncs.vote_2("Vice President", vp_candidates, vp_vote_cols, final_votes)
-
 	print(ec_result)
+
+	ePR_vote_cols = [3, 4, 5]
+	ePR_candidates = [re.findall(regEx, first_row[x])[0] for x in ePR_vote_cols]
+	ePR_result = voteFuncs.vote_3("External PR", ePR_candidates, ePR_vote_cols, final_votes)
 	print(ePR_result)
-	print(vp_result)
+
+	iPR_vote_cols = [6, 7, 8, 9]
+	iPR_candidates = [re.findall(regEx, first_row[x])[0] for x in iPR_vote_cols]
+	iPR_result = voteFuncs.vote_4("Internal PR", iPR_candidates, iPR_vote_cols, final_votes)
+	print(iPR_result)
